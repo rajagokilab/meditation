806856216c5d0ab367349c12349d6e1168f48c47
Rajagokila B <rajagokilab@gmail.com>
2025-08-28 14:36:14 +0200

    initial

--- meditation_site/urls.py
+++ meditation_site/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('meditation.urls')),
 ]
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
